7beb5f27826ebc3b367e7cde2cef1ee70adef470
miqbdi79 <iqbalabdi07@gmail.com>
2021-07-01 07:28:19 +0700

    fix auth

--- app/Http/Controllers/MyTryOutController.php
+++ app/Http/Controllers/MyTryOutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\RegisterTryout;
 use App\Models\Tryout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,9 +13,9 @@ class MyTryoutController extends Controller
 {
     public function index()
     {
-      
+
 //         abort_if(Gate::denies('mytryouts_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-        
+
 //         $mytryouts = RegisterTryout::all();
 //         return view('mytryouts.index', compact('mytryouts'));
                 //
