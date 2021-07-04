db22313cfdd3afb47fd7c80f5eb5e0d3f5c94bc2
dws-in <dwi.wahyu.toso@gmail.com>
2021-07-05 01:09:14 +0700

    update ranking

--- resources/views/ranking/show.blade.php
+++ resources/views/ranking/show.blade.php
@@ -32,7 +32,7 @@
                                             class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             English
                                         </th>
-                                       @if($rankings[0]->cluster_id == 1)
+                                       @if($rankings[1]->cluster_id == 1)
                                         <th scope="col"
                                             class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Mathematic
